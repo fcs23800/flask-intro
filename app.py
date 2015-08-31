25fd5b5c37fe1d7c7cd25c7a137ee02f14319081
Nupur Patel <npatel@fcsinet.com>
2015-08-31 16:30:00 -0700

    convert app to blueprints

--- app.py
+++ app.py
@@ -1,64 +1,63 @@
-# import the flask class from the Flask module
-from flask import Flask, render_template, redirect, url_for, request, session, flash
+#################
+#### imports ####
+#################
+
+from flask import Flask, flash, redirect, session, url_for, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
 from functools import wraps
-from flask.ext.sqlalchemy import SQLAlchemy 
-
-# create the application object
-app = Flask(__name__)
-
-# config
 import os
-app.config.from_object(os.environ['APP_SETTINGS'])
-print os.environ['APP_SETTINGS']
 
-# create the sqlalchemy object
+################
+#### config ####
+################
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
-# import db schema
 from models import *
+from project.users.views import users_blueprint
 
-#login required decorator
-def login_required(f):
-	@wraps(f)
-	def wrap(*args, **kwargs):
-		if 'logged_in' in session:
-			return f(*args, **kwargs)
-		else:
-			flash('You need to login first.')
-			return redirect(url_for('login'))
-	return wrap
+# register our blueprints
+app.register_blueprint(users_blueprint)
 
-# use decorators to link the function to a url - Home page
+##########################
+#### helper functions ####
+##########################
+
+
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('users.login'))
+    return wrap
+
+
+################
+#### routes ####
+################
+
+# use decorators to link the function to a url
 @app.route('/')
 @login_required
 def home():
-	# return "Hello, World!"  # return a string
-	posts = db.session.query(BlogPost).all()
-	return render_template('index.html', posts=posts)  # render a template
+    # return "Hello, World!"  # return a string
+    posts = db.session.query(BlogPost).all()
+    return render_template('index.html', posts=posts)  # render a template
+
 
 @app.route('/welcome')
 def welcome():
-	return render_template('welcome.html')
+    return render_template('welcome.html')  # render a template
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	error = None
-	if request.method == 'POST':
-		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-			error = 'Invalid Credentials. Please try again.'
-		else:
-			session['logged_in'] = True
-			flash('You were logged in.')
-			return redirect(url_for('home'))
-	return render_template('login.html', error=error)
 
-@app.route('/logout')
-@login_required
-def logout():
-	session.pop('logged_in', None)
-	flash('You were logged out.')
-	return redirect(url_for('welcome'))
+####################
+#### run server ####
+####################
 
-# start the server with the run() method
 if __name__ == '__main__':
-	app.run()
+    app.run()
