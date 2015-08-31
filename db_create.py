e7a8a8cce4a1349960b684c27178ba4bf3f650ef
Nupur Patel <npatel@fcsinet.com>
2015-08-31 15:02:15 -0700

    added migrations

--- db_create.py
+++ db_create.py
@@ -9,6 +9,8 @@ db.session.add(BlogPost("Good", "I\'m good."))
 db.session.add(BlogPost("Well", "I\'m well."))
 db.session.add(BlogPost("Excellent", "I\'m excellent."))
 db.session.add(BlogPost("Okay", "I\'m okay."))
+db.session.add(BlogPost("Flask", "Go to learningflask.com to see the magic"))
+db.session.add(BlogPost("Postgres", "set up new postgresql database discover flask dev locally."))
 
 # commit the changes
 db.session.commit()
