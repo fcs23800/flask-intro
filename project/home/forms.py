e6224b207e3601336378bbcf806406548482c30c
Nupur Patel <npatel@fcsinet.com>
2015-09-02 18:44:08 -0700

    Added solid test framework

---
+++ project/home/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import Form
+from wtforms import TextField, TextAreaField
+from wtforms.validators import DataRequired, Length
+
+
+class MessageForm(Form):
+	title = TextField('Title', validators=[DataRequired()])
+	description = TextAreaField('Description', validators=[DataRequired(), Length(min=6, max=4096)])
--- project/home/views.py
+++ project/home/views.py
@@ -2,34 +2,27 @@
 #### imports ####
 #################
 
-from project import app, db
-from project.models import BlogPost
-from flask import flash, redirect, session, url_for, render_template, Blueprint
-from functools import wraps
+from project import db   # pragma: no cover
+from project.models import BlogPost   # pragma: no cover
+from forms import MessageForm   # pragma: no cover
+
+from flask import render_template, Blueprint, flash, url_for, redirect, request   # pragma: no cover
+from flask.ext.login import login_required, current_user   # pragma: no cover
+
+#from project import app, db
+#from project.models import BlogPost
+#from flask import flash, redirect, session, url_for, render_template, Blueprint
+#from flask.ext.login import login_required
+#from functools import wraps
 
 ################
 #### config ####
 ################
 
 home_blueprint = Blueprint(
-    'home', __name__,
-    template_folder='templates'
+	'home', __name__,
+	template_folder='templates'
 )
-
-##########################
-#### helper functions ####
-##########################
-
-
-def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('users.login'))
-    return wrap
 
 
 ################
@@ -37,14 +30,27 @@ def login_required(test):
 ################
 
 # use decorators to link the function to a url
-@home_blueprint.route('/')
+@home_blueprint.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    # return "Hello, World!"  # return a string
-    posts = db.session.query(BlogPost).all()
-    return render_template('index.html', posts=posts)  # render a template
+	error = None
+	form = MessageForm(request.form)
+	if form.validate_on_submit():
+		new_message = BlogPost(
+			form.title.data,
+			form.description.data,
+			current_user.id
+		)
+		db.session.add(new_message)
+		db.session.commit()
+		flash('New entry was successfully posted. Thanks.')
+		return redirect(url_for('home.home'))
+	else:
+		posts = db.session.query(BlogPost).all()
+		return render_template(
+			'index.html', posts=posts, form=form, error=error)
 
 
 @home_blueprint.route('/welcome')
 def welcome():
-    return render_template('welcome.html')  # render a template
+	return render_template('welcome.html')  # render a template
